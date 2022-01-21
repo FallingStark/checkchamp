34511678cbf881c9ced56309a9a2726d7e2a8cb7
FallingStark <59834046+FallingStark@users.noreply.github.com>
2022-01-21 19:46:44 +0100

    add get champ mastery

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ def main():
     """Demande quelle option l'user veut utilisé"""
     choice_bool = False
 
-    SummonerName = input("Quelle est ton summoner name ? ")
+    #SummonerName = input("Quelle est ton summoner name ? ")
+    SummonerName = "FallingSt4rk"
     SummonerData = Player(SummonerName)
     if SummonerData.code != 200:
         print(f"Code : {SummonerData.code}")
@@ -19,17 +20,20 @@ def main():
         print("Que veut tu faire")
         print("Check tes mastery [1]")
         print("Check mastery score [2]")
+        print("Ceck champion mastery [3]")
         try:
             choice = int(input(""))
         except:
             choice = 0
-        if 1 <= choice <= 2:
+        if 1 <= choice <= 3:
             choice_bool = True
     print(f"ton choice est {choice}")
     if choice == 1:
         get_mastery(SummonerData)
     if choice == 2:
         get_mastery_score(SummonerData)
+    if choice == 3:
+        get_champion_mastery(SummonerData)
 
 
 def get_mastery(SummonerData):
@@ -45,6 +49,9 @@ def get_mastery(SummonerData):
 def get_mastery_score(SummonerData):
     content = SummonerData.get_all_mastery()
     print(f"Total champion mastery score : {content}")
+
+def get_champion_mastery(SummonerData):
+    content = SummonerData.get_champion_mastery("pyke")
 
 
 if __name__ == "__main__":
--- stats.py
+++ stats.py
@@ -4,6 +4,7 @@ import os
 from pprint import pprint
 import requests
 from dotenv import load_dotenv
+import json
 
 __version__ = '1.0'
 
@@ -44,3 +45,17 @@ class Player:
         if self.code == 200:
             content = response.json()
             return content
+    def get_champion_mastery(self, champion_wanted_name):
+        with open("champion_id.json", "r") as file:
+            file_content = file.read()
+        list_champion = json.loads(file_content)
+        for champion_id in list_champion:
+            champion = list_champion[champion_id]
+            if champion.lower() == champion_wanted_name.lower():
+                champion_wanted_id = champion_id
+        request_mastery_champion = f"https://euw1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/{self.id}/by-champion/{champion_wanted_id}"
+        response = requests.get(request_mastery_champion, headers=Header)
+        self.code = response.status_code
+        if self.code == 200:
+            content = response.json()
+            print(content)
