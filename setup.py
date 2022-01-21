b70f304e0f415aa38efb9b7af9b37d552d24d2fa
FallingStark <59834046+FallingStark@users.noreply.github.com>
2022-01-21 11:05:19 +0100

    update

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='antipackage',
+      version='1.0',
+      author="FallingStark",
+      author_email="mr.thematthieu007@gmail.com",
+      license="MIT License",
+      url="https://github.com/FallingStark/checkchamp",
+      py_modules=['antipackage'],
+)
